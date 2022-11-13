8feb17b32a35221ccf89e2daefe2d0dfdbff4635
CNP HUB <85221307+ToonGamer@users.noreply.github.com>
2022-11-13 21:10:20 +0700

    Delete fff.py

--- fff.py
+++
@@ -1,1 +0,0 @@
-roblox-player:1+launchmode:play+gameinfo:{ticket}+launchtime:{int(time()*1000)}+placelauncherurl:{PlaceLauncherURL}+browsertrackerid:{browserid}+robloxLocale:en_us+gameLocale:en_us+channel:
