aabced3ce7f82e1b78ccc48d74e18fb7a57846e3
CNP HUB <85221307+ToonGamer@users.noreply.github.com>
2022-11-13 21:09:45 +0700

    Create fff.py

---
+++ fff.py
@@ -0,0 +1,1 @@
+roblox-player:1+launchmode:play+gameinfo:{ticket}+launchtime:{int(time()*1000)}+placelauncherurl:{PlaceLauncherURL}+browsertrackerid:{browserid}+robloxLocale:en_us+gameLocale:en_us+channel:
